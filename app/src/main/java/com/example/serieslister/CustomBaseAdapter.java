53ec260092d13f8eaeac1c50e6ceeb99ae73a385
Sjoerd van der Helm <sjoerd.van.der.helm@postnl.nl>
2023-06-24 12:33:44 +0200

    List viewer

---
+++ app/src/main/java/com/example/serieslister/CustomBaseAdapter.java
@@ -0,0 +1,52 @@
+package com.example.serieslister;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Arrays;
+
+public class CustomBaseAdapter extends BaseAdapter {
+
+    Context ctx;
+    String[] series;
+    int[] seriesImages;
+    LayoutInflater layoutInflater;
+
+    public CustomBaseAdapter(Context ctx, String[] series, int[] seriesImages) {
+        this.ctx = ctx;
+        this.series = series;
+        this.seriesImages = seriesImages;
+        this.layoutInflater = LayoutInflater.from(ctx);
+
+    }
+
+    @Override
+    public int getCount() {
+        return series.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return Arrays.stream(series).findFirst();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        convertView = layoutInflater.inflate(R.layout.activity_custom_list_viewer, null);
+        TextView textView = (TextView) convertView.findViewById(R.id.textImage);
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.imageIcon);
+        textView.setText(series[position]);
+        imageView.setImageResource(seriesImages[position]);
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/serieslister/MainActivity.java
+++ app/src/main/java/com/example/serieslister/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.serieslister;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
@@ -13,7 +14,12 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    String series[] = {"Sopranos", "The Wire"};
+    int seriesImages[] = {R.drawable.sopranos, R.drawable.the_wire};
+
     public ConstraintLayout constraintLayout;
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +31,9 @@ public class MainActivity extends AppCompatActivity {
 
         Toast.makeText(this, "May thee be happy", Toast.LENGTH_LONG).show();
         constraintLayout.setVisibility(View.VISIBLE);
+
+        listView = (ListView) findViewById(R.id.seriesListView);
+        CustomBaseAdapter customBaseAdapter = new CustomBaseAdapter(getApplicationContext(), series, seriesImages);
+        listView.setAdapter(customBaseAdapter);
     }
 }
